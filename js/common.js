d4385448aad10489056bc0aaf3395ef5ac5554f1
wujingchao <wujingchao92@gmail.com>
2018-01-21 14:32:10 +0800

    Site updated: 2018-01-21 14:32:09

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() { 
+     $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto;');
+     prettyPrint();
+
+	  $(".diagram_sequence").sequenceDiagram({theme: 'simple'});
+      var size = $(".diagram_flow").size();
+      for (var i = 0; i < size; i ++) {
+          var node = $(".diagram_flow").get(i);
+          var textContent = node.textContent;
+          node.textContent = null;
+          node.id = "canvas" + i;
+          $("#" + node.id).css("visibility","visible");
+          var chart = flowchart.parse(textContent);
+          chart.drawSVG(node.id,{
+            'line-width': 1.5,
+          });
+      }
+}); 
